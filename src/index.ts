9f2e007b94bcdc4abe2a1ef07d33292bf1849809
Bozsoki Levente <levbozsoki@gmail.com>
2023-11-14 08:55:58 +0100

    v2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { StrategyVerifyCallback } from "remix-auth";
+import { StrategyVerifyCallback } from 'remix-auth';
 import type {
-  OAuth2Profile,
-  OAuth2StrategyVerifyParams,
-} from "remix-auth-oauth2";
-import { OAuth2Strategy } from "remix-auth-oauth2";
+    OAuth2Profile,
+    OAuth2StrategyVerifyParams,
+} from 'remix-auth-oauth2';
+import { OAuth2Strategy } from 'remix-auth-oauth2';
 
-export type LinkedInScope = "openid" | "profile" | "email";
+export type LinkedInScope = 'openid' | 'profile' | 'email';
 
 /**
  * This type declares what configuration the strategy needs from the
@@ -13,14 +13,14 @@ export type LinkedInScope = "openid" | "profile" | "email";
  * @see {@link https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow LinkedIn Auth Flow}
  */
 export type LinkedInStrategyOptions = {
-  clientID: string;
-  clientSecret: string;
-  callbackURL: string;
-  /**
-   * @default "openid profile email"
-   * @see {@link https://docs.microsoft.com/en-us/linkedin/shared/authentication/authentication?context=linkedin/context#permission-types Permisision}
-   */
-  scope?: LinkedInScope[] | string;
+    clientID: string;
+    clientSecret: string;
+    callbackURL: string;
+    /**
+     * @default "openid profile email"
+     * @see {@link https://docs.microsoft.com/en-us/linkedin/shared/authentication/authentication?context=linkedin/context#permission-types Permisision}
+     */
+    scope?: LinkedInScope[] | string;
 };
 
 /**
@@ -28,131 +28,137 @@ export type LinkedInStrategyOptions = {
  * based on the information Linkedin gives us.
  */
 export interface LinkedInProfile extends OAuth2Profile {
-  id: string;
-  displayName: string;
-  name: {
-    givenName: string;
-    familyName: string;
-  };
-  emails: Array<{ value: string }>;
-  photos: Array<{ value: string }>;
-  _json: {
-    sub: string;
-    name: string;
-    given_name: string;
-    family_name: string;
-    picture: string;
-    locale: string;
-    email: string;
-    email_verified: boolean;
-  };
+    id: string;
+    displayName: string;
+    name: {
+        givenName: string;
+        familyName: string;
+    };
+    emails: Array<{ value: string }>;
+    photos: Array<{ value: string }>;
+    _json: {
+        sub: string;
+        name: string;
+        given_name: string;
+        family_name: string;
+        picture: string;
+        locale: string;
+        email: string;
+        email_verified: boolean;
+    };
 }
 
 export type LinkedInExtraParams = {
-  scope: string;
+    scope: string;
 } & Record<string, string | number>;
 
 export const LinkedInStrategyDefaultScopes: LinkedInScope[] = [
-  "openid",
-  "profile",
-  "email",
+    'openid',
+    'profile',
+    'email',
 ];
-export const LinkedInStrategyDefaultName = "linkedin";
-export const LinkedInStrategyScopeSeparator = " ";
+export const LinkedInStrategyDefaultName = 'linkedin';
 
-export class LinkedinStrategy<User> extends OAuth2Strategy<
-  User,
-  LinkedInProfile,
-  LinkedInExtraParams
+export const LinkedInStrategyScopeSeparator = ' ';
+
+export class LinkedInStrategy<User> extends OAuth2Strategy<
+    User,
+    LinkedInProfile,
+    LinkedInExtraParams
 > {
-  public name = LinkedInStrategyDefaultName;
+    public name = LinkedInStrategyDefaultName;
 
-  private readonly scope: LinkedInScope[];
-  private readonly redirect_uri: string;
-  private readonly userInfoURL = "https://api.linkedin.com/v2/userinfo";
+    // Change the type of `scope` to `string`
+    protected readonly scope: string;
+    private readonly redirect_uri: string;
+    private readonly userInfoURL = 'https://api.linkedin.com/v2/userinfo';
 
-  constructor(
-    {
-      clientID,
-      clientSecret,
-      callbackURL,
-      scope = LinkedInStrategyDefaultScopes,
-    }: LinkedInStrategyOptions,
-    verify: StrategyVerifyCallback<
-      User,
-      OAuth2StrategyVerifyParams<LinkedInProfile, LinkedInExtraParams>
-    >
-  ) {
-    super(
-      {
-        clientID,
-        clientSecret,
-        callbackURL,
-        authorizationURL: "https://www.linkedin.com/oauth/v2/authorization",
-        tokenURL: "https://www.linkedin.com/oauth/v2/accessToken",
-      },
-      verify
-    );
+    constructor(
+        {
+            clientID,
+            clientSecret,
+            callbackURL,
+            scope = LinkedInStrategyDefaultScopes,
+        }: LinkedInStrategyOptions,
+        verify: StrategyVerifyCallback<
+            User,
+            OAuth2StrategyVerifyParams<LinkedInProfile, LinkedInExtraParams>
+        >
+    ) {
+        super(
+            {
+                clientID,
+                clientSecret,
+                callbackURL,
+                authorizationURL:
+                    'https://www.linkedin.com/oauth/v2/authorization',
+                tokenURL: 'https://www.linkedin.com/oauth/v2/accessToken',
+            },
+            verify
+        );
 
-    this.scope = this.getScope(scope);
-    this.redirect_uri = callbackURL;
-  }
-
-  // Allow users the option to pass a scope string, or typed array
-  private getScope(scope: LinkedInStrategyOptions["scope"]) {
-    if (!scope) {
-      return LinkedInStrategyDefaultScopes;
-    } else if (typeof scope === "string") {
-      return scope.split(LinkedInStrategyScopeSeparator) as LinkedInScope[];
+        // Store `scope` as a string
+        this.scope = this.getScope(scope);
+        this.redirect_uri = callbackURL;
     }
 
-    return scope;
-  }
-  /**
-   * We override the protected authorizationParams method to return a new
-   * URLSearchParams with custom params we want to send to the authorizationURL.
-   * Here we add the scope and the redirect_uri so Linkedin can use it, you can pass any extra param
-   * you need to send to the authorizationURL here base on your provider.
-   *
-   * @see {@link https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?tabs=HTTPS#step-2-request-an-authorization-code}
-   * @returns {string} The new URL Search params
-   */
-  protected authorizationParams(): URLSearchParams {
-    return new URLSearchParams({
-      scope: this.scope.join(LinkedInStrategyScopeSeparator),
-      redirect_uri: this.redirect_uri,
-      response_type: "code",
-    });
-  }
+    // Allow users the option to pass a scope string
+    private getScope(scope: LinkedInStrategyOptions['scope']): string {
+        if (!scope) {
+            return LinkedInStrategyDefaultScopes.join(
+                LinkedInStrategyScopeSeparator
+            );
+        } else if (typeof scope === 'string') {
+            return scope;
+        }
 
-  /**
-   * We override how to use the accessToken to get the profile of the user.
-   * Here we fetch a Linkedin specific URLs, get the profile data, the user email
-   * and build the object based on the LinkedinProfile type.
-   *
-   * @see {@link https://docs.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin#retrieving-member-email-address Retrieving Email Address}
-   * @param accessToken
-   * @returns {LinkedInProfile} The Linkedin profile and the raw json response.
-   */
-  protected async userProfile(accessToken: string): Promise<LinkedInProfile> {
-    const response = await fetch(this.userInfoURL, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const raw: LinkedInProfile["_json"] = await response.json();
-    const profile: LinkedInProfile = {
-      provider: "linkedin",
-      id: raw.sub,
-      displayName: raw.name,
-      name: {
-        familyName: raw.family_name,
-        givenName: raw.given_name,
-      },
-      emails: [{ value: raw.email }],
-      photos: [{ value: raw.picture }],
-      _json: raw,
-    };
-    return profile;
-  }
+        return scope.join(LinkedInStrategyScopeSeparator);
+    }
+    /**
+     * We override the protected authorizationParams method to return a new
+     * URLSearchParams with custom params we want to send to the authorizationURL.
+     * Here we add the scope and the redirect_uri so Linkedin can use it, you can pass any extra param
+     * you need to send to the authorizationURL here base on your provider.
+     *
+     * @see {@link https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?tabs=HTTPS#step-2-request-an-authorization-code}
+     * @returns {string} The new URL Search params
+     */
+    protected authorizationParams(): URLSearchParams {
+        return new URLSearchParams({
+            scope: this.scope.toString(),
+            redirect_uri: this.redirect_uri,
+            response_type: 'code',
+        });
+    }
+
+    /**
+     * We override how to use the accessToken to get the profile of the user.
+     * Here we fetch a Linkedin specific URLs, get the profile data, the user email
+     * and build the object based on the LinkedinProfile type.
+     *
+     * @see {@link https://docs.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin#retrieving-member-email-address Retrieving Email Address}
+     * @param accessToken
+     * @returns {LinkedInProfile} The Linkedin profile and the raw json response.
+     */
+    protected async userProfile(accessToken: string): Promise<LinkedInProfile> {
+        const response = await fetch(this.userInfoURL, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const raw: LinkedInProfile['_json'] = await response.json();
+        const profile: LinkedInProfile = {
+            provider: 'linkedin',
+            id: raw.sub,
+            displayName: raw.name,
+            name: {
+                familyName: raw.family_name,
+                givenName: raw.given_name,
+            },
+            emails: [{ value: raw.email }],
+            photos: [{ value: raw.picture }],
+            _json: raw,
+        };
+        return profile;
+    }
 }
